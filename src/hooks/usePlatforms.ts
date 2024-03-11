4a67e5fbb173a0ff59425a3d2555172258b13b93
Albi Dema <adema@ritech.co>
2024-03-11 10:57:03 +0100

    Filter games by platform

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -5,6 +5,6 @@ interface Platform {
     name: string;
     slug: string;
 }
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents2')
+const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
 
 export default usePlatforms;
