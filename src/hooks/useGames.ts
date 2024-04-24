bbb2101dcf702c0b58e918b360623fdd69e26a41
Albi Dema <adema@ritech.co>
2024-04-24 11:30:09 +0200

    Implement Searching

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,4 @@
 import useData from "./useData";
-import {Genre} from "./useGenres";
 import {GameQuery} from "../App";
 
 export interface Platform {
@@ -7,6 +6,7 @@ export interface Platform {
     name: string;
     slug: string;
 }
+
 export interface Game {
     id: number,
     name: string;
@@ -15,6 +15,16 @@ export interface Game {
     metacritic: number;
 }
 
-const useGames = (gameQuery: GameQuery) => useData<Game>('/games', {params: {genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder}}, [gameQuery])
+const useGames = (gameQuery: GameQuery) =>
+    useData<Game>('/games',
+        {
+            params: {
+                genres: gameQuery.genre?.id,
+                platforms: gameQuery.platform?.id,
+                ordering: gameQuery.sortOrder,
+                search: gameQuery.searchText,
+            }
+        },
+        [gameQuery])
 
 export default useGames;
